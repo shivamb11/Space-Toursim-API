dcee4818e190679f7648c578e3f7c51c4cbcb34d
Shivam Bansal <shivamsb2003@gmail.com>
2024-04-25 16:07:27 +0530

    main

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ if (process.env.NODE !== "production") {
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const tourRouter = require("./routes");
 
@@ -14,6 +15,7 @@ async function main() {
   await mongoose.connect(process.env.MONGO_URL);
 }
 
+app.use(cors());
 app.use(express.json());
 app.use("/", tourRouter);
 
